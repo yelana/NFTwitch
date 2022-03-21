f4fe39bb2cbbd60d75fed57dec67d58a5b93e897
yelana <44828517+yelana@users.noreply.github.com>
2022-03-21 16:41:01 -0700

    Create BaseContract.sol

---
+++ BaseContract.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.7;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+
+contract AIGenFT is ERC721URIStorage {
+    uint256 public tokenCounter;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIDCounter;
+
+    constructor() ERC721("BurnCollection","WBC") {}
+    
+    function createToken(string memory tokenURI) public returns (uint256) {        
+        _tokenIDCounter.increment();
+        uint256 newItemID = _tokenIDCounter.current();
+        _safeMint(msg.sender, newItemID);
+        _setTokenURI(newItemID, tokenURI);        
+        return newItemID;
+    }
+    
+ }
